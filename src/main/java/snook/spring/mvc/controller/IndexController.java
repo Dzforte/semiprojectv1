eaf8baf337da98708ca53d2fbb2f970d6845566e
Jeong <Jeong@DESKTOP-07CFLN6>
2022-09-28 17:51:06 +0900

    index page design

--- src/main/java/snook/spring/mvc/controller/IndexController.java
+++ src/main/java/snook/spring/mvc/controller/IndexController.java
@@ -1,15 +1,19 @@
 package snook.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+
+@Controller
 public class IndexController {
-
 	
 	@GetMapping("/")
-	public String index() {
-		return "Hello, World!!";
+	public String index(Model m) {
+		
+		m.addAttribute("sayHello", "Hello, World!!");  // 헬로 월드를 세이 헬로에 담아서 보냄
+		
+		return "index";
 	}
 }
