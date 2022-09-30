2edd2aef96c39b3fcc92e49fadf9b7ecdb9e5b32
Dzforte <dzforte@gmail.com>
2022-09-30 12:40:23 +0900

    memberDAO 쿼리 수정

--- src/main/java/snook/spring/mvc/dao/BoardDAOImpl.java
+++ src/main/java/snook/spring/mvc/dao/BoardDAOImpl.java
@@ -2,6 +2,8 @@ package snook.spring.mvc.dao;
 
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -9,22 +11,22 @@ import snook.spring.mvc.vo.BoardVO;
 
 @Repository("bdao")
 public class BoardDAOImpl implements BoardDAO {
-
 	
+	//@AutoWired   // bean 태그에 정의한 경우 생략 가능
 	private JdbcTemplate jdbcTemplate;	
 	
-
+	private SimpleJdbcInsert simpleInsert;
+	
+	public BoardDAOImpl(DataSource dataSource) {
+		simpleInsert = new SimpleJdbcInsert(dataSource).withTableName("board").usingColumns("title", "userid", "contents");
+	}
 	
 	@Override
 	public int insertBoard(BoardVO bvo) {
 		
-		String sql = "insert into board (title, userid, contents)" + "values (?,?,?)";
+		SqlParameterSource params = new BeanPropertySqlParameterSource(bvo);		
+		return simpleInsert.execute(params);
 		
-		Object[] params = new Object[] {
-				bvo.getTitle(), bvo.getUserid(), bvo.getContents()
-		};
-		
-		return jdbcTemplate.update(sql, params);
 	}
 	
 
--- src/main/java/snook/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/snook/spring/mvc/dao/MemberDAOImpl.java
@@ -1,7 +1,11 @@
 package snook.spring.mvc.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import snook.spring.mvc.vo.MemberVO;
@@ -9,21 +13,16 @@ import snook.spring.mvc.vo.MemberVO;
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO {
 
-	@Autowired
+	private SimpleJdbcInsert simpleInsert;
 	private JdbcTemplate jdbcTemplate;
 
+	public MemberDAOImpl(DataSource dataSource) {
+		simpleInsert = new SimpleJdbcInsert(dataSource).withTableName("member").usingColumns("userid", "passwd", "name", "email");
+	}
+	
 	@Override
 	public int insertMember(MemberVO mvo) {
-
-		String sql = "insert into member " + "(userid, passwd, name, email) " + "values (?, ?, ?, ?) ";
-
-		//
-		Object[] params = new Object[] {
-
-				mvo.getUserid(), mvo.getPasswd(), mvo.getName(), mvo.getEmail() };
-
-		return jdbcTemplate.update(sql, params);
-
-	};
-
+		SqlParameterSource params = new BeanPropertySqlParameterSource(mvo);		
+		return simpleInsert.execute(params);
+	}
 }
